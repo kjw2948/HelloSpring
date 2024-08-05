ba56accfc0b05020e2019494c6f8e2991be6b387
kjw2948 <kjw970313@cau.ac.kr>
2024-08-05 15:43:40 +0900

    [Update] 요청 파라미터 - 컨트롤러 사용

---
+++ src/main/java/hello/springmvc/request/RequestParamController.java
@@ -0,0 +1,82 @@
+package hello.springmvc.request;
+
+import hello.springmvc.HelloData;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+@Slf4j
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username = {}, age = {}", username, age);
+
+        response.getWriter().write("ok");
+    }
+
+
+    @RequestMapping("/request-param-v2")
+    @ResponseBody
+    public String requestParamV2(
+            @RequestParam("username") String memberName, // () 들어가는 이름이 url이나 body에 들어있는 요청변수명
+            @RequestParam("age") int memberAge) {
+        log.info("username = {}, age = {}", memberName, memberAge);
+        return "Ok";
+    }
+
+    @RequestMapping("/request-param-v3")
+    @ResponseBody
+    public String requestParamV3(
+            @RequestParam() String username, // () 들어가는 이름이 url이나 body에 들어있는 요청변수명과 동일하면 생략 가능
+            @RequestParam() int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "Ok";
+    }
+
+    @RequestMapping("/request-param-v4")
+    @ResponseBody
+    public String requestParamV4(String username, int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "Ok";
+    }
+
+    // 요청 파라미터 Map으로 조회하기
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+
+        log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@RequestParam String username, @RequestParam int age) {
+        HelloData helloData = new HelloData();
+        helloData.setUsername(username);
+        helloData.setAge(age);
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        return "ok ";
+    }
+
+
+    //요청 파라미터 -> ModelAttribute 사용
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(@ModelAttribute HelloData helloData) {
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        return "ok ";
+    }
+}
